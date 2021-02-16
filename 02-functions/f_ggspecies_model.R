0b5602d9858cc5d8b43252b6707f727bd3b73373
Julen Astigarraga <julenastigarraga@gmail.com>
2021-02-16 11:06:31 +0100

    more advanced eexamples

---
+++ 02-functions/f_ggspecies_model.R
@@ -0,0 +1,51 @@
+ggspecies <- function (sp.id) {
+  
+  #' @description 
+  #' @author Julen Astigarraga
+  #' @param sp.id
+  #' @return 
+  
+  ggplot(df, aes(x = prod_year,
+             y = prod_value,
+             color = sites)) +
+    geom_point(size = 0.1) + 
+    scale_color_viridis_d() +
+    scale_y_continuous(expand = c(0, 0)) +
+    scale_x_discrete(labels = c("1", "2", "3")) +
+    coord_cartesian(ylim = c(0, 120)) +
+    ylab("Productivity") + 
+    facet_grid(.~sites, switch = "x") +  
+    gghighlight(species %in% sp.id,
+                calculate_per_facet = TRUE,
+                use_direct_label = FALSE) +
+    ggtitle(sp.id, ) + 
+    theme(legend.position = "none",
+          title = element_text(colour = "black", size = 5),
+          axis.title = element_text(colour = "black", size = 4),
+          axis.text.y = element_text(colour = "black", size = 4),
+          axis.text.x = element_text(colour = "black", size = 2),
+          axis.title.x = element_blank(),
+          axis.ticks = element_blank(),
+          panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank(),
+          panel.background = element_blank(),
+          axis.line = element_line(colour = "black", size = 0.5), 
+          strip.background = element_blank(),
+          strip.placement = "outside",
+          strip.text.x = element_text(size = 4))
+
+}
+
+
+
+
+purrr_lm <- function(df){
+  
+  #' @description 
+  #' @author Julen Astigarraga
+  #' @param df
+  #' @return 
+  
+  lm(data = df, prod_value ~ species)
+  
+}
