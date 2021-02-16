ee94a16dc781c2e9c8c552d7ac0c8a2294ab5b9e
Julen Astigarraga <julenastigarraga@gmail.com>
2021-02-16 18:22:37 +0100

    separate the functions

--- 02-functions/f_ggspecies.R
+++ 02-functions/f_ggspecies.R
@@ -37,15 +37,3 @@ ggspecies <- function (sp.id) {
 }
 
 
-
-
-purrr_lm <- function(df){
-  
-  #' @description fit a linear model using species id as explanatory variable and productivity value as response variable
-  #' @author Julen Astigarraga
-  #' @param df
-  #' @return a linear model
-  
-  lm(data = df, prod_value ~ species)
-  
-}
